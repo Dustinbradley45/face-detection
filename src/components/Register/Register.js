6c4efebac0f5752ce4cf9cf30d29f16541f503fb
Dustin Bradley <dustinbradley@Dustins-MacBook-Air.local>
2019-04-04 12:09:52 -0400

    added thigns

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import "./SignIn.css";
+import React from "react";
+import "./../SignIn/SignIn.css";
 
-const SignIn = (props) => {
+const Register = (props) => {
   return (
   <article className="br3 tc ba dark-gray b--black-10 mv4 w-100 shadow-5 w-50-m w-25-l mw6 center">
   <main className="pa4 black-80">
     <form className="measure center">
       <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+        <legend className="f1 fw6 ph0 mh0">Register</legend>
         <div className="mt3">
-          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-        </div>
+          <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="email">E-Mail Address</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email"  id="email"/>
+          </div>
         <div className="mv3">
           <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
           <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
@@ -20,9 +24,7 @@ const SignIn = (props) => {
       <div className="center">
             <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" onClick={props.onRouteChange}/>
       </div>
-      <div className="lh-copy mt3">
-            <p className="f6 link dim black db" onClick={props.onRegister}>Register</p>
-      </div>
+
       </form>
       </main>
     </article>
@@ -30,4 +32,4 @@ const SignIn = (props) => {
     );
 }
 
-export default SignIn;
+export default Register;
